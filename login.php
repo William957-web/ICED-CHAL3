187dc23c6544595cc7eecce67f1e9ff55e318068
William957-web <85293841+William957-web@users.noreply.github.com>
2024-01-28 14:23:30 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['username'])&&isset($_POST['password'])){
-    if($_POST['username']!='40000'&&$_POST['password']!='JoyLife'){
+    if($_POST['username']!='40000'||$_POST['password']!='JoyLife'){
            echo('<h1>Login Failed</h1>');
     }
     else{
